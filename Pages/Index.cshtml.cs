34494167efe40b4994f41682cf90d21017411a26
Jonathan Ramanujam <75343684+jonathanramanujam@users.noreply.github.com>
2023-08-03 21:46:37 -0600

    Homepage displays list of items

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,20 +1,51 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Kadince_Todo_Ramanujam.Data;
+using Kadince_Todo_Ramanujam.Models;
 
 namespace Kadince_Todo_Ramanujam.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        private readonly Kadince_Todo_Ramanujam.Data.Kadince_Todo_RamanujamContext _context;
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(Kadince_Todo_Ramanujam.Data.Kadince_Todo_RamanujamContext context)
         {
-            _logger = logger;
+            _context = context;
         }
 
-        public void OnGet()
-        {
+        [BindProperty]
+        public IList<TodoItem> TodoItems { get; set; } = default!;
 
+        public async Task OnGetAsync()
+        {
+            if (_context.TodoItem != null)
+            {
+                TodoItems = await _context.TodoItem.ToListAsync();
+            }
+        }
+
+        public string TimeAlive(DateTime DateCreated)
+        {
+            TimeSpan span = DateTime.Now - DateCreated;
+            if (span.Days > 0)
+            {
+                return $"Added {span.Days} days ago";
+            }
+            else if (span.Hours > 0)
+            {
+                return $"Added {span.Hours} hours ago";
+            }
+            else if (span.Hours > 0)
+            {
+                return $"Added {span.Minutes} minutes ago";
+            }
+            return "This item has transcended time itself.";
         }
     }
 }
